70110d20d164cc55c002b5b1a9f4a644a1511f22
JiaruiFeng <feng.jiarui@wustl.edu>
2024-09-25 15:37:27 -0500

    save ckpt

--- convert_ckpt.py
+++ convert_ckpt.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
                      "saved_exp/2024-09-23 16:21:43.771065_117/GOFA/aoe9ip3b/checkpoints/best_ckptepoch=1-step=6279.ckpt",]
 
     save_path_list = ["cache_data/mem_ckpt_3_4209.pth",
-                      "cache_data/mem_ckpt_2_5243.pth",
-                      "cache_data/mem_ckpt_2_6279.pth"]
+                      "cache_data/mem_ckpt_3_5243.pth",
+                      "cache_data/mem_ckpt_3_6279.pth"]
 
     for load_dir, save_path in zip(load_dir_list, save_path_list):
         convert_ckpt(load_dir, save_path)
